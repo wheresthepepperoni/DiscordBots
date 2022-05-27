a4735db8a748f6840a62112fd0e4ac5123f5fce6
root <root@ip-172-31-22-156.ec2.internal>
2022-05-27 18:20:27 +0000

    Added MorbBot

---
+++ Discord/Prod/MorbBot.py
@@ -0,0 +1,36 @@
+# MorbBot.py
+
+import os
+import discord
+import random
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('MORB_DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+
+@client.event
+async def on_ready():
+    print(
+    f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    morb = ['It\'s Morbin\' time',
+'Just admiring my Morb',
+'#MORBIUSSWEEP']
+
+    morbImage = 'image'
+
+    if 'morb' in message.content.lower() or 'morbius' in message.content.lower():
+        response = random.choice(morb)
+        await message.channel.send(response)
+
+client.run(TOKEN)
